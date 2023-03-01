37089d0bad6cf4c38cd5c8fa9355032892088238
Mateo Shkodra <66566003+mateedo@users.noreply.github.com>
2023-03-01 02:32:59 -0500

    Add files via upload

---
+++ dlmaps.sh
@@ -0,0 +1,12 @@
+#! /bin/bash/
+
+for map in *.osz; do
+  mv "$map" "${map}.zip"
+  mv "${map}.zip" ../documents/osu/Songs/
+  unzip ../documents/osu/Songs/"${map}.zip" -d ../Documents/osu/Songs/"$map"
+done
+
+for skin in *.osk; do
+  mv "$skin" "${skin}.zip"
+  mv "${skin}.zip" ../Documents/osu/Skins/
+  unzip ../Documents/osu/Skins/"${skin}.zip" -d ../Documents/osu/Skins/"$skin"
